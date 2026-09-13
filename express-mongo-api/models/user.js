6f99373fc5c29bd60b65a19a17753036bbb41316
Fahma Ali <halbeeg.accandconsultancy@gmail.com>
2026-09-13 17:02:18 +0300

    auth

--- express-mongo-api/models/user.js
+++ express-mongo-api/models/user.js
@@ -1,8 +1,27 @@
 import mongoose from 'mongoose';
-const { Schema } = mongoose;
-const userchemaShape = new Schema({
+// const { Schema } = mongoose;
+// const userchemaShape = new Schema({
+//     name: String,
+//     email:String
+// })
+// const users = mongoose.model("User", userchemaShape);
+
+const userSchema = new mongoose.Schema({
     name: String,
-    email:String
+    email: { type: String, unique: true },
+    password:String
 })
-const users = mongoose.model("User", userchemaShape);
-export default users
+// Hash password before saving means did not update
+userSchema.pre('save', async function (next) {
+  //the password was not modified
+  if (!this.isModified("password")) return next(); //Stop this middleware and continue saving.
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+// Method to compare password
+userSchema.method.comparePassword = function (inputPassword) {
+    return bcrypt.compare(inputPassword,this.password)
+}
+const Users= mongoose.model('Users',userSchema)
+export default Users;
