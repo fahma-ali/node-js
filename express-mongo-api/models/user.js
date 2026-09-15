2248251ec9d180d3375c79a0352bcf965f057ff7
fahma ali <asilentco@gmail.com>
2026-09-15 06:57:50 +0300

    user

--- express-mongo-api/models/user.js
+++ express-mongo-api/models/user.js
@@ -1,8 +1,17 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 const { Schema } = mongoose;
 const userchemaShape = new Schema({
-    name: String,
-    email:String
+    name: { type: String ,required:true},
+    email: { type: String, required: true, unique: true },
+    password: String
+
+})
+userchemaShape.pre('save',async function (next) {
+    if (!this.isModified(this.password)) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password= await bcrypt.hash(this.password,salt)
 })
 const users = mongoose.model("User", userchemaShape);
 export default users
