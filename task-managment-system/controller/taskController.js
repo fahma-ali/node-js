a1912bef30e0fd3a37dbd3c1281ee63a41c7e12c
Fahma Ali <halbeeg.accandconsultancy@gmail.com>
2026-09-22 16:31:03 +0300

    task operations

--- task-managment-system/controller/taskController.js
+++ task-managment-system/controller/taskController.js
@@ -11,3 +11,40 @@ export const createTask = async(req, res, next) => {
         next(error)
     }
 }
+
+export const getMyTask = async(req, res, next) => {
+    try {
+        const task = await Task.find({createdBy:req.user._id});
+        if (!task) return res.status(404).send("not found task");
+        res.json(task);
+    } catch (error) {
+        next(error)
+    }
+}
+export const updateTask = async(req, res, next) => {
+    try {
+        const task = await Task.findOneAndUpdate(
+          { _id: req.params.id, createdBy: req.user._id },
+          req.body,
+          { returnDocument: true },
+        );
+        if(!task) return res.status(404).json({message:"Task not found"})
+        res.json({message:"Updated successfully",task})
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteTask = async (req, res, next) => {
+    try {
+            const task = await Task.findOneAndDelete({
+              _id: req.params.id,
+              createdBy: req.user._id,
+            });
+            if(!task) return res.status(404).json({message:"Task not found"})
+   
+             res.json({message:"Deleted Successfuly"})   
+    } catch (error) {
+        next(error)
+    }
+}
--- task-managment-system/route/task.js
+++ task-managment-system/route/task.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { createTask } from '../controller/taskController.js';
+import { createTask, deleteTask, getMyTask, updateTask } from '../controller/taskController.js';
 import { protect } from '../middleWare/auth.js';
 const router = express.Router();
 router.post('/',protect, createTask)
+router.get("/", protect, getMyTask);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, deleteTask);
 
 export default router
